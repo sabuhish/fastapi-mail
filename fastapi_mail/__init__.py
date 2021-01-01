d46968ab0efb009606f90b9f60651254857c13c7
Sabuhi <sabuhi.shukurov@gmail.com>
2021-01-02 00:51:02 +0400

    Update __init__.py

--- fastapi_mail/__init__.py
+++ fastapi_mail/__init__.py
@@ -2,7 +2,7 @@
 from fastapi_mail.fastmail import FastMail
 from fastapi_mail.version import VERSION
 from fastapi_mail.config import  ConnectionConfig
-from fastapi_mail.schemas import MessageSchema
+from fastapi_mail.schemas import MessageSchema,MultipartSubtypeEnum
 from . import email_utils
 
 
@@ -12,5 +12,5 @@ __author__ = "sabuhi.shukurov@gmail.com"
 
 
 __all__ = [
-    "FastMail", "VERSION", "ConnectionConfig", "MessageSchema", "email_utils"
+    "FastMail", "VERSION", "ConnectionConfig", "MessageSchema", "email_utils", "MultipartSubtypeEnum"
 ]
