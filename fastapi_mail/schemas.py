a6e9ff9cb9d8406747f3c20377ac258f33c1de95
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2025-10-16 14:40:16 +0600

    pydantic-v2

--- fastapi_mail/schemas.py
+++ fastapi_mail/schemas.py
@@ -91,15 +91,22 @@ class MessageSchema(BaseModel):
         return temp
 
     @model_validator(mode="after")
-    def validate_alternative_body(cls, values):
+    def validate_alternative_body(self):
         """
         Validate alternative_body field
         """
+        # if (
+        #     values.multipart_subtype != MultipartSubtypeEnum.alternative
+        #     and values.alternative_body
+        # ):
+        #     values.alternative_body = None
+        # return values
+        # pydantic:v2 wants self to return:
         if (
-            values.multipart_subtype != MultipartSubtypeEnum.alternative
-            and values.alternative_body
+            self.multipart_subtype != MultipartSubtypeEnum.alternative
+            and self.alternative_body
         ):
-            values.alternative_body = None
-        return values
+            self.alternative_body = None
+        return self
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
